b112150c18c91c91b36c681eec74ca46a3492d63
Loc Nguyen <loc.nguyen@holistics.io>
2017-11-21 23:04:27 +0700

    Setup Front-End project

---
+++ app/components/Layout/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './layout.css';
+
+class Navigate extends React.Component {
+ 
+  render() {
+    return(
+      <div>
+        <h1>Hello I am Layout</h1>
+      </div>
+    );
+  }
+}
+
+export default Navigate;
---
+++ app/components/LeftSideBar/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LeftSideBar extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello I am a Left SideBar</h1>
+      </div>
+    );
+  }
+}
+
+export default LeftSideBar;
